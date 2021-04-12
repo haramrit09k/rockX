d9dcf2dcd752a1ab893fc648d0cb16c1970d6714
Haramrit Khurana <haramrit09k@gmail.com>
2021-04-11 23:52:16 -0400

    edit sendFile and static path in server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static('dist'));
+app.use(express.static('dist/rockX'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join('dist/index.html'));   
+    res.sendFile(path.join('dist/rockX/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
